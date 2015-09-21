c51e25ed4b67a94a329aac59af64198a8de832a1
Landon Brenner <landon.brenner@gmail.com>
2015-09-21 12:27:09 -0700

    added favicon.ico

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('navbar-fixed-top', { title: 'Fleet Manager 0.1' });
+  // res.render('navbar-fixed-top', { title: 'Fleet Manager 0.1' });
+  res.render('index', {title: 'Fleet Manager 0.1'});
 });
 
 module.exports = router;
